c77ffff702da2f679de969611f46ca534238da64
Bjarne Varöystrand <black.skorpio+github@gmail.com>
2019-01-17 22:49:53 +0100

    Addinjg a demo mode and check

--- Fireworks/js/fireworks.js
+++ Fireworks/js/fireworks.js
@@ -15,7 +15,8 @@ var footWrap	= "fw_foot";
 
 var devHost		= 'oxygen.local';
 var deBugState	= 0;
-var demoState	= 1;
+var demoHost	= 'demo.kokensupport.com';
+var demoState	= 0;
 var debugHash	= 'fireworksDebug';
 var demoHash	= 'fireworksDemo';
 
@@ -31,7 +32,7 @@ var endDate		= new Date( endValue + '/' + NewYear );
 
 var dateCheck	= currentTime > startDate && currentTime < endDate;
 var debugCheck	= deBugState == 1 || ( location.hostname == devHost || location.hash == '#'+debugHash );
-var demoCheck	= demoState == 1 && ( location.hash == '#'+demoHash );
+var demoCheck	= ( demoState == 1 || location.hash == '#'+demoHash ) || location.hostname == demoHost;
 
 var FwFooter		= document.getElementById( footWrap );
 var FireworkText	= document.getElementById( textWrapp );
@@ -41,24 +42,83 @@ var FireworkCanvas	= document.getElementById( canvasID );
    https://sharepoint.stackexchange.com/a/218571 */
 var FireworksKey	= "KokenFireWorks";
 var FireworksValue	= NewYear + '.'+ location.hostname;
+var storageCheck = sessionStorage.getItem( FireworksKey ) !== FireworksValue;
 
 function theBroom() {
 	if( FwFooter !=null ) FwFooter.remove();
 	if( FireworkText !=null ) FireworkText.remove();
 	if( FireworkCanvas !=null ) FireworkCanvas.remove();
-	if ( debugCheck ) console.log('cleaning');
+	if ( debugCheck ) console.info('Cleaning up after the Fireworks');
 };
 
 if ( debugCheck ) {
-	if ( dateCheck ) {
-		console.log( currentTime + ' is in range ' + startDate + ' and ' + endDate );
-	} else {
-		console.log( currentTime + ' is not between ' + startDate + ' and ' + endDate );
+	function deBugger() {
+		var _csl			= console;
+		var consoleHeader	= 'font:1.5em sans-serif; color:orange;';
+		var consoleBody		= 'font:1 sans-serif;';
+		var consoleOkClr	= 'color:green;';
+		var consoleNopeClr	= 'color:red;';
+		var consoleDivider	= '\n--------------------------------------------------\n\n';
+
+		_csl.debug('%c-=- Koken Fireworks is in debug mode -=-', consoleHeader );
+
+		if ( dateCheck ) {
+			_csl.debug('%c' + currentTime + ' is in range ' + startDate + ' and ' + endDate, consoleBody + consoleOkClr, consoleDivider );
+		} else {
+			_csl.debug('%c' + currentTime + ' is not between ' + startDate + ' and ' + endDate, consoleBody + consoleNopeClr, consoleDivider );
+		}
+
+		if ( demoCheck ) _csl.debug('%c-=- Koken Fireworks is in demo mode -=-', consoleHeader );
+
+		_csl.debug('%c-=- The Element ID\'s -=-', consoleHeader );
+		_csl.debug('canvasID: #' + canvasID + '\n' +
+		'textWrapp: #' + textWrapp + '\n' +
+		'yearID: #' + yearID + '\n' +
+		'footWrap: #' + footWrap +
+		consoleDivider );
+
+		_csl.debug('%c-=- Debug info -=-', consoleHeader );
+		_csl.debug('devHost: ' + devHost + '\n' +
+		'deBugState: ' + deBugState + '\n' +
+		'debugHash: #' + debugHash + '\n' +
+		'demoState: ' + demoState + '\n' +
+		'demoHost: ' + demoHost + '\n' +
+		'demoHash: #' + demoHash +
+		consoleDivider);
+
+		_csl.debug('%c-=- Checks -=-', consoleHeader );
+		_csl.debug('dateCheck: ' + dateCheck + '\n' +
+		'debugCheck: ' + debugCheck + '\n' +
+		'demoCheck: ' + demoCheck + '\n' +
+		'spanYear: ' + spanYear +
+		consoleDivider);
+
+		_csl.debug('%c-=- Dates and timings -=-', consoleHeader );
+		_csl.debug('currentTime: ' + currentTime + '\n' +
+		'currentYear: ' + currentYear + '\n' +
+		'NewYear: ' + NewYear + '\n' +
+		'startDate: ' + startDate + '\n' +
+		'endDate: ' + endDate + '\n' +
+		'waitTime: ' + waitTime + 'ms' + '\n' +
+		'fadeTime: ' + fadeTime + 'ms' + '\n' +
+		'fireworksTime: ' + fireworksTime + 'ms' +
+		consoleDivider);
+
+		_csl.debug('%c-=- Objects -=-', consoleHeader );
+		_csl.debug('FwFooter: ' + FwFooter + '\n' +
+		'FireworkText: ' + FireworkText + '\n' +
+		'FireworkCanvas: ' + FireworkCanvas +
+		consoleDivider);
+
+		_csl.debug('%c-=- Session Storage -=-', consoleHeader );
+		_csl.debug('FireworksKey: ' + FireworksKey + '\n' +
+		'FireworksValue: ' + FireworksValue );
 	}
-	if ( demoCheck ) console.log('Koken Fireworks is in demo mode');
+	deBugger();
 }
 
-if ( demoCheck || dateCheck && sessionStorage.getItem( FireworksKey ) !== FireworksValue ) {
+//if ( ( demoCheck && sessionStorage.getItem( FireworksKey ) !== FireworksValue ) || ( dateCheck && sessionStorage.getItem( FireworksKey ) !== FireworksValue ) ) {
+if ( ( demoCheck || dateCheck ) && storageCheck ) {
 	var gc = new GameCanvas( canvasID );
 
 	var year;
